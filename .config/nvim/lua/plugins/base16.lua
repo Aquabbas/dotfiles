ebf4666c7308cfdd33da7698845dde684c5a56f1
Aquabbas <abbashayder@hotmail.com>
2024-07-01 10:05:33 +0200

    refactor(gitignore, plugin): remove plugin and extend gitignore

--- .config/nvim/lua/plugins/base16.lua
+++ .config/nvim/lua/plugins/base16.lua
@@ -1,7 +1,7 @@
 return {
-	"RRethy/base16-nvim",
-	-- after = 'lazy.nvim',  -- Load after 'lazy.nvim'
-	config = function()
-		vim.cmd("colorscheme base16-catppuccin-mocha")
-	end,
+-- 	"RRethy/base16-nvim",
+-- 	-- after = 'lazy.nvim',  -- Load after 'lazy.nvim'
+-- 	config = function()
+-- 		vim.cmd("colorscheme base16-catppuccin-mocha")
+-- 	end,
 }
