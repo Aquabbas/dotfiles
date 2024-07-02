7c1b4017760849c8b52d1e2dc5c44ff313819a9d
Aquabbas <abbas.hayder@odevo.com>
2024-07-02 19:42:40 +0200

    refactor(kanagawa): remove unnecessary tuple

--- .config/nvim/lua/plugins/kanagawa.lua
+++ .config/nvim/lua/plugins/kanagawa.lua
@@ -1,12 +1,10 @@
 return {
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000, -- Adjust priority as needed
-		name = "kanagawa",
-		config = function()
-			require("kanagawa").setup()
-			vim.cmd("colorscheme kanagawa-wave")
-		end,
-	},
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000, -- Adjust priority as needed
+    name = "kanagawa",
+    config = function()
+        require("kanagawa").setup()
+        vim.cmd("colorscheme kanagawa-wave")
+    end,
 }
