71698ed84dd8b4148bbe4695d0d1104898af1303
Aquabbas <abbas.hayder@odevo.com>
2024-07-02 18:59:13 +0200

    refactor(plugins): deactivate plugins and remove extra themes

--- .config/nvim/lua/plugins/rosepine.lua
+++ .config/nvim/lua/plugins/rosepine.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"EdenEast/nightfox.nvim",
-		lazy = false,
-		priority = 1000,
-		name = "Terafox Theme",
-		config = function()
-			require("nightfox").load({
-				variant = "terafox",
-			})
-			vim.cmd("colorscheme nightfox")
-		end,
-	},
+	-- {
+	-- 	"EdenEast/nightfox.nvim",
+	-- 	-- lazy = false,
+	-- 	-- priority = 1000,
+	-- 	-- name = "Terafox Theme",
+	-- 	-- config = function()
+	-- 	-- 	require("nightfox").load({
+	-- 	-- 		variant = "terafox",
+	-- 	-- 	})
+	-- 	-- 	vim.cmd("colorscheme nightfox")
+	-- 	-- end,
+	-- },
 }
