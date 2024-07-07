521224a8000af3ff327e7974d0c4cce5ff0de56e
Aquabbas <abbashayder@hotmail.com>
2024-07-07 13:28:02 +0200

    refactor(neovim, keymaps): configure vim options
    Addd keymaps for staying in indent mode after indenting. add shortcut
    (Ctrl + Arrow Key) for resizing windows. Highlight text after yank for
    approximatately 300 ms.

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -14,11 +14,28 @@ vim.wo.relativenumber = true
 -- For obsidian.nvim
 vim.opt.conceallevel = 2
 
--- Set Python 3 host program
-vim.g.python3_host_prog = "/usr/bin/python3"
+-- Visual --
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
--- Set Ruby host program (using symlinked path)
-vim.g.ruby_host_prog = "/usr/bin/ruby"
+-- Resize with arrows
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", { desc = "Resize Horizontal Split Down" })
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", { desc = "Resize Horizontal Split Up" })
+-- vim.keymap.set("n", "<C-S-Down>", ":resize +2<CR>", { desc = "Resize Horizontal Split Down" })
+-- vim.keymap.set("n", "<C-S-Up>", ":resize -2<CR>", { desc = "Resize Horizontal Split Up" })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Resize Vertical Split Down" })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Resize Vertical Split Up" })
+
+-- highlight yanks
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 300, -- duration in milliseconds
+		})
+	end,
+})
 
 -- Enable Ctrl + hjkl for pane navigation
 -- vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
@@ -31,3 +48,9 @@ vim.g.ruby_host_prog = "/usr/bin/ruby"
 -- vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
 -- vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
 -- vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
+-- Set Python 3 host program
+vim.g.python3_host_prog = "/usr/bin/python3"
+
+-- Set Ruby host program (using symlinked path)
+vim.g.ruby_host_prog = "/usr/bin/ruby"
