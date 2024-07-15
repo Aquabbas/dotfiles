702192cea958621cc7fcf4dfb0fa16196a3add19
Aquabbas <abbas.hayder@odevo.com>
2024-07-15 14:30:48 +0200

    refactor(plugin): reformat telescope plugin config

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,19 +1,11 @@
 return {
 	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.8",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>s", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>b", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
-			vim.keymap.set("n", "<leader>r", builtin.oldfiles, {})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
 				extensions = {
@@ -22,6 +14,14 @@ return {
 					},
 				},
 			})
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>s", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
+			vim.keymap.set("n", "<leader>r", builtin.oldfiles, {})
+
 			require("telescope").load_extension("ui-select")
 		end,
 	},
