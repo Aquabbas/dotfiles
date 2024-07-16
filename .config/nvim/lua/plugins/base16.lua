39f6e09e04f87c685512559502a7129f6b1402d0
Aquabbas <abbashayder@hotmail.com>
2024-07-16 14:46:04 +0200

    style(plugins): add back base16 plugin

--- .config/nvim/lua/plugins/base16.lua
+++ .config/nvim/lua/plugins/base16.lua
@@ -1,7 +1,9 @@
 return {
--- 	"RRethy/base16-nvim",
--- 	-- after = 'lazy.nvim',  -- Load after 'lazy.nvim'
--- 	config = function()
--- 		vim.cmd("colorscheme base16-catppuccin-mocha")
--- 	end,
+	"RRethy/base16-nvim",
+	after = "lazy.nvim", -- Load after 'lazy.nvim'
+	config = function()
+		vim.cmd("colorscheme base16-kanagawa")
+	end,
 }
+-- base16-kanagawa
+-- base16-catppuccin-mocha
